857eb9bf68e97e3fa1480168ccd2bf770de017fc
DSobscure <snbruce13@yahoo.com.tw>
2017-09-24 22:56:23 +0800

    HX Register

--- Door_of_Soul.Client/ClientSystem.cs
+++ Door_of_Soul.Client/ClientSystem.cs
@@ -1,14 +1,11 @@
 ﻿using Door_of_Soul.Communication.Client.System;
 using Door_of_Soul.Core.Client;
 using Door_of_Soul.Core.Protocol;
-using System;
 
 namespace Door_of_Soul.Client
 {
     public class ClientSystem : VirtualSystem
     {
-        public event Action<ClientAnswer> OnProxyAnswerLoaded;
-
         public override OperationReturnCode Register(string answerName, string basicPassword, out string errorMessage)
         {
             if (answerName.Length < 1 || answerName.Length > 20)
@@ -36,16 +33,6 @@ namespace Door_of_Soul.Client
                 SystemOperationRequestApi.Login(answerName, basicPassword);
                 errorMessage = "";
                 return OperationReturnCode.Successiful;
-            }
-        }
-
-        public override void LoadProxyAnswer(int answerId, string answerName, int[] soulIds)
-        {
-            ClientAnswer answer;
-            if(AnswerFactory.Instance.Create(answerId, answerName, soulIds, out answer))
-            {
-                VirtualAnswer.Initialize(answer);
-                OnProxyAnswerLoaded?.Invoke(answer);
             }
         }
     }
